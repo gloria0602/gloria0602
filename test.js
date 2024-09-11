d0880c607ad4384abe68f7ab055ac266760ec0e3
gloria <gloriatchiaze06@gmail.com>
2024-09-11 10:47:00 +0100

    fitrst commit

---
+++ test.js
@@ -0,0 +1,13 @@
+let listemot=["baton","prune","patate"];
+let score =0;
+let i=0;
+let mot="";
+for(i=0;i!==3;i++){
+mot=prompt("entrez le 1er mot\n");
+if(mot==listemot[i]){
+    score=score+1;
+}
+}
+console.log("your score is :",score);
+    
+
